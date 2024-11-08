d9743a7962e9ee8b8fa6a5f87968d7546b1aa916
KibuuleNoah <noahkibuule3@gmail.com>
2024-11-08 15:02:12 +0300

    remove .env

--- DjangoVault/settings.py
+++ DjangoVault/settings.py
@@ -148,11 +148,3 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "notrikissoftware@gmail.com"
-EMAIL_HOST_PASSWORD = "vtmd saib xnhb wywf"
-DEFAULT_FROM_EMAIL = "notrikissoftware@gmail.com"
